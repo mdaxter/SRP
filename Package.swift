b52a92727100f589f26fa0765b712228bc8892b0
Fred Rajaona <frajaona@gmail.com>
2016-10-15 23:16:25 +0200

    Use alternative Bignum package

--- Package.swift
+++ Package.swift
@@ -4,6 +4,6 @@ let package = Package(
     name: "SRP",
     dependencies: [
         .Package(url: "https://github.com/Bouke/CommonCrypto.git", majorVersion: 1),
-        .Package(url: "https://github.com/Bouke/Bignum.git", majorVersion: 1),
+        .Package(url: "https://github.com/frajaona/Bignum.git", majorVersion: 1),
     ]
 )
